a9356e9f8ce5ccc2515aec9161ddaedd419d801c
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-07-13 09:32:54 +0300

    + class Status(TextChoices) + field status

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique=True)
     body = models.TextField()
@@ -9,6 +12,7 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
 
     class Meta:
         ordering = ['-publish']
