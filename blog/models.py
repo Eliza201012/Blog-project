22658e12c020f533d92d2eeb391599bed4870a8f
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-07-27 11:37:56 +0300

    import reverse, change field slug from unique to unique_for_date, add def get_absolute_url()

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -10,7 +11,7 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique_for_date=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts', null=True)
     body = models.TextField()
 
@@ -30,3 +31,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
