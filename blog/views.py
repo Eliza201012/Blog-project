7fa0845f75c488c21ee81f0abd95b2e26b04e2e5
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-07-29 17:50:07 +0300

    import Paginator, реалізована посторінкова розбивка з 3 постами на сторінку

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Посторінкова розбивка з 3 постами на сторінку
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request, "blog/post/list.html", {"posts" : posts})
 
 def post_detail(request, year, month, day, post):
