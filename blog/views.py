0538a9e607d7c8ed25aced8a7ce937cac4565fc6
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-07-29 19:30:15 +0300

    import PageNotAnInteger, обробляємо якщо користувач ввів не ціле число

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
     post_list = Post.published.all()
     # Посторінкова розбивка з 3 постами на сторінку
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
+
     try:
         posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Якщо page_number не ціле число, то
+        # показати першу сторінку
+        posts = paginator.page(1)
     except EmptyPage:
         # Якщо page_number знаходиться поза межами діапазону, то
         # показати останню сторінку
         posts = paginator.page(paginator.num_pages)
+
     return render(request, "blog/post/list.html", {"posts" : posts})
 
 def post_detail(request, year, month, day, post):
