0a50ffd6d6dc9cb3119159278820c0e58d0780f0
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-07-29 19:09:23 +0300

    import EmptyPage, обробляємо якщо користувач ввів некоректні дані

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 
 def post_list(request):
     post_list = Post.published.all()
     # Посторінкова розбивка з 3 постами на сторінку
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # Якщо page_number знаходиться поза межами діапазону, то
+        # показати останню сторінку
+        posts = paginator.page(paginator.num_pages)
     return render(request, "blog/post/list.html", {"posts" : posts})
 
 def post_detail(request, year, month, day, post):
