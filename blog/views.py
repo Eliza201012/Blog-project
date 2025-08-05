965debed7bb2db2b4c7eb0098e46e79cf68aac77
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-08-05 19:43:09 +0300

    змінено з HttpResponseRedirect на просто redirect

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import EmailPostForm
 from django.core.mail import send_mail
 from django.conf import settings
-from django.http import HttpResponseRedirect
 
 def post_list(request):
     post_list = Post.published.all()
@@ -53,7 +52,7 @@ def post_share(request, post_id):
                       [cd['to']])
             sent = True
             # ... надіслати електронний лист
-        return HttpResponseRedirect("/")
+        return redirect("blog:post_list")
     else:
         form = EmailPostForm()
         return render(request, 'blog/post/share.html', {'post':post, 'form':form, 'sent':sent})
